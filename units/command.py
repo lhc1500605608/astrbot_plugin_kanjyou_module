7cdc528f7055ef893ca98281091c5a95ddaec643
lhc1500605608 <lhc1500605608@hotmail.com>
2026-04-05 15:56:30 +0800

    修复指令冲突

---
+++ units/command.py
@@ -0,0 +1,7 @@
+"""Compatibility shim for legacy singular module path.
+This file intentionally contains no command decorators.
+"""
+
+
+class CommandUnitsMixin:
+    pass
---
+++ units/unit_command.py
@@ -0,0 +1,7 @@
+"""Compatibility shim for legacy module path.
+This file intentionally contains no command decorators.
+"""
+
+
+class CommandUnitsMixin:
+    pass
